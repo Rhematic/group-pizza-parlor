6a9c450f0339a723b5de723c8c1b0973719a246f
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-09-11 18:20:58 -0500

    remove stack

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -61,8 +61,6 @@ function App() {
         </Route>
       </Router>
 
-      <img src="images/pizza_photo.png" />
-      <p>Pizza is great.</p>
     </div>
   );
 }
--- src/components/SelectPizza/SelectPizza.jsx
+++ src/components/SelectPizza/SelectPizza.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
-import SelectPizzaItem from "../SelectPizzaItem/SelectPizzaItem";
-import CustomBox from "../Box/Box";
-
+import React from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+import "./SelectPizza.css"
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const SelectPizza = () => {
   const pizzas = useSelector((store) => store.pizzas);
+  const dispatch = useDispatch();
 
-  return pizzas.map((pizza) => (
-    <div key={pizza.id}>
-      <SelectPizzaItem pizza={pizza} />
+  const addPizzaToCart = (pizza) => {
+    dispatch({ type: "ADD_TO_CART", payload: pizza });
+  };
+
+  return (
+    <div className="pizza-stack-container">
+      {pizzas.map((pizza) => (
+        <Stack key={pizza.id} className="pizza-stack">
+          <Item>
+            <img src={pizza.image_path} alt={pizza.name} />
+            <h2>{pizza.name}</h2> {pizza.description} <br />
+            <b>${pizza.price}</b>
+            <br />
+            <button onClick={() => addPizzaToCart(pizza)}>Add to Cart</button>
+          </Item>
+        </Stack>
+      ))}
     </div>
-  ));
+  );
 };
 
 export default SelectPizza;
