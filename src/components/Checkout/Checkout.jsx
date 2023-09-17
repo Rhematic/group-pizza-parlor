8e8765d9f5db6da6fc15536e90884ce0d062330e
Adam Myhand <rhematicmovement@gmail.com>
2023-09-17 18:44:05 -0500

    Checkout-page-added

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
 
-function Checkout (){
+function Checkout() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const order = useSelector((store) => store.order);
+  const customer = useSelector((store) => store.customer);
+
+  const submitOrder = () => {
+    Axios.post("/api/order", customer)
+      .then((response) => {
+        console.log(response);
+        dispatch({ type: "CLEAR_ORDER" });
+        dispatch({ type: "CLEAR_CUSTOMER" });
+        history.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-      <div>
-        <h1>Checkout test 1</h1>
-      </div>
-    );
+    <div className="Checkout">
+      <h1>Step 3: Checkout</h1>
+      <h3>{customer.customer_name}</h3>
+      <h3>{customer.street_address}</h3>
+      <h3>{customer.city}</h3>
+      <h3>{customer.zip}</h3>
+      <h3>{customer.type}</h3>
+      <h3>{customer.total}</h3>
+
+      {/* map the order as a table */}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {order.map((pizza) => {
+            return (
+              <tr key={pizza.id}>
+                <td>{pizza.name}</td>
+                <td>{pizza.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={submitOrder}>Submit Order</button>
+    </div>
+  );
 }
 
 export default Checkout;
