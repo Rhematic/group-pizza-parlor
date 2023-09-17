acd68b67ed09b5b498964dcd9041698dac490c13
Zach Wright <zwright101@gmail.com>
2023-09-17 18:21:44 -0500

    added admin component to group repo

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
+
+function Admin() {
+    const dispatch = useDispatch();
+    const orderList = useSelector(store => store.orderList);
+
+    const fetchOrders = () => {
+        axios.get('/api/order').then((response) => {
+            dispatch({ type: 'FETCH_ORDERS', payload: response.data});
+        }).catch((error) => {
+            console.log('Error fetching orders' , error);
+        });
+    }
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
+
+    return (
+        <div>
+            <table id='orders'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Type</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orderList.map((order) => (
+                            <tr key={order.id}>
+                                <td>{order.customer_name}</td>
+                                <td>{order.time}</td>
+                                <td>{order.type}</td>
+                                <td>{order.total}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default Admin
+export default Admin;
