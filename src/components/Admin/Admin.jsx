8c73764f613a10248a006392cdf1ca12ae6da641
Adam Myhand <rhematicmovement@gmail.com>
2023-09-17 19:07:09 -0500

    bug fixes done

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,49 +2,49 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 function Admin() {
-    const dispatch = useDispatch();
-    const orderList = useSelector(store => store.orderList);
+  const dispatch = useDispatch();
+  const orderList = useSelector((store) => store.order);
 
-    const fetchOrders = () => {
-        axios.get('/api/order').then((response) => {
-            dispatch({ type: 'FETCH_ORDERS', payload: response.data});
-        }).catch((error) => {
-            console.log('Error fetching orders' , error);
-        });
-    }
+  const fetchOrders = () => {
+    axios
+      .get("/api/order")
+      .then((response) => {
+        dispatch({ type: "SET_ORDER", payload: response.data });
+      })
+      .catch((error) => {
+        console.log("Error fetching orders", error);
+      });
+  };
 
-    useEffect(() => {
-        fetchOrders();
-    }, []);
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
-    return (
-        <div>
-            <table id='orders'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Time</th>
-                        <th>Type</th>
-                        <th>Cost</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        orderList.map((order) => (
-                            <tr key={order.id}>
-                                <td>{order.customer_name}</td>
-                                <td>{order.time}</td>
-                                <td>{order.type}</td>
-                                <td>{order.total}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table id="orders">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Time</th>
+            <th>Type</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orderList.map((order) => (
+            <tr key={order.id}>
+              <td>{order.customer_name}</td>
+              <td>{order.time}</td>
+              <td>{order.type}</td>
+              <td>{order.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Admin;
